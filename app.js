e2468de5315b2ddd39805faf721bb32a3c61705f
Divyansh Dixit <ddivyansh18@gmail.com>
2021-08-23 01:49:19 +0530

    Added CSS and fetch call

--- app.js
+++ app.js
@@ -5,9 +5,26 @@ var outputDiv = document.querySelector("#output")
 console.log(outputDiv)
 console.log(txtInput)
 
+var serverURL = "	https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+    console.log("Error logged : " + error);
+}
+
 
 function clickHandler() {
-    outputDiv.innerText = "translated: " + txtInput.value;
+    inputText = txtInput.value;
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => outputDiv.innerText = json.contents.translated)
+    .catch(errorHandler)
+
 }
+
+
 
 btnTranslate.addEventListener("click", clickHandler);
